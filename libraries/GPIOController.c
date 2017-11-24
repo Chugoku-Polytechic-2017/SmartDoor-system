ccc70a1b98f1ebe342360dbcc9d7f08aed6ccf21
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-11-24 15:21:22 +0900

    GPIOController.hのinclude はせずに、標準ライブラリをインクルード

--- libraries/GPIOController.c
+++ libraries/GPIOController.c
@@ -1,4 +1,9 @@
-#include "GPIOController.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
 
 int pinMode(char *path, int isIN){
     int fd, status;
