db9a072e7fa8401d6cb44669e9b2fba7823854fe
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-11-24 14:16:16 +0900

    必要なライブラリーをインクルード

--- libraries/GPIOController.h
+++ libraries/GPIOController.h
@@ -1,4 +1,16 @@
+#ifndef GPIOController_H
+#define GPIOController_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <limits.h>
+
 #define OUTPUT 1
 #define INPUT 0
 
-int PinMode(char *pin, int mode);
+int pinMode(char *pin, int mode);
+
+#endif
