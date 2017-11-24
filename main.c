fda4c439faaad88f391c297eec4d74cbef827168
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-11-24 15:47:02 +0900

    LED点滅プログラム追加

--- main.c
+++ main.c
@@ -4,5 +4,11 @@
 int main(void) {
     char *path = "/sys/class/gpio/CON9_11/";
     pinMode(path, OUTPUT);
+    while(1){
+        digitalWrite(path, HIGH);
+        sleep(3);
+        digitalWrite(path, LOW);
+        sleep(3);
+    }
     return;
 }
