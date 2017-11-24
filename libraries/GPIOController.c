d6c5bcc70283a8a55931c22d4b18f0a0b9e1b098
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-11-24 14:19:02 +0900

    pinMode関数の定義

--- libraries/GPIOController.c
+++ libraries/GPIOController.c
@@ -1,2 +1,25 @@
 #include "GPIOController.h"
 
+int pinMode(char *path, int isIN){
+    int fd, status;
+    char direction[50] = "";
+    char edge[40] = "";
+    strcat(direction, path);
+    strcat(edge, path);
+    strcat(direction, "direction");
+    strcat(edge, "edge");
+
+    fd = open(direction, O_WRONLY);
+    if (fd == -1) return EXIT_FAILURE;
+    if (isIN) {
+        status = write(fd, "in", 2);
+    } else {
+        status = write(fd, "out", 3);
+    }
+    close(fd);
+    fd = open(edge, O_WRONLY);
+    if (fd == -1) return EXIT_FAILURE;
+    status = write(fd, "none", 1);
+    close(fd);
+    return status;
+}
