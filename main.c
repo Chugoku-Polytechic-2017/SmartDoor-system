7ae9b6b86fa3dce55d41419aa15a27094fe2e97f
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-11-24 15:15:58 +0900

    modifed main function's return value

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "libraries/GPIOController.h"
 
-void main(void) {
+int main(void) {
     char *path = "/sys/class/gpio/CON9_11/";
     pinMode(path, OUTPUT);
     return;
