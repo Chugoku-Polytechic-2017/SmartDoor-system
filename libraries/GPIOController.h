4a807d6cff23dae6382f945b7ac2334cb7fae191
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-11-24 15:22:04 +0900

    GPIOController.hでは標準ライブラリを読みこまない

--- libraries/GPIOController.h
+++ libraries/GPIOController.h
@@ -1,13 +1,6 @@
 #ifndef GPIOController_H
 #define GPIOController_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <limits.h>
-
 #define OUTPUT 1
 #define INPUT 0
 
