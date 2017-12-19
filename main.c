f072cb6f257fad5ce8fe723b9bc3415ef70342f4
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-12-19 12:23:46 +0900

    add samplecode

--- main.c
+++ main.c
@@ -5,21 +5,26 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <limits.h>
-
+#include "libraries/GPIOController.h"
 
 int main(void){
     int fd_red1 = -1, fd_red2 = -1, fd_green1 = -1, fd_green2 = -1, loop, status_code = 0;
     char *path[PORTS];
-
-    path[0] = "/sys/class/gpio/CON9_11/";
+    int status;
     
-    pinMode(path[0], 0);
+    path[0] = COM9_11;
+    
+    pinMode(path[0], OUTPUT);
 
     while (1) {
-        digitalWrite(path[0], 0);
+        digitalWrite(path[0], LOW);
+        status = digitalRead(path[0]);
+        printf("%d\r\n",status);
         sleep(3);
-        digitalWrite(path[0], 1);
+        digitalWrite(path[0], HIGH);
+        status = digitalRead(path[0]);
+        printf("%d\r\n",status);
         sleep(3);
     }
     return status_code;
-}
+} 
