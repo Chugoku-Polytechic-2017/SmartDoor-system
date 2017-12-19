40d210bcb87c88913e46f24313822dbc1a4befea
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-12-19 12:50:12 +0900

    GPIOピンの定義追加

--- libraries/GPIOController.h
+++ libraries/GPIOController.h
@@ -5,11 +5,22 @@
 #include <fcntl.h>
 #include <limits.h>
 
-#define OUTPUT 1
-#define INPUT 0
+#define OUTPUT 0
+#define INPUT 1
 #define HIGH 1
 #define LOW 0
 #define PORTS 8
+#define COM9_11 "/sys/class/gpio/CON9_11/";
+#define COM9_12 "/sys/class/gpio/CON9_12/";
+#define COM9_13 "/sys/class/gpio/CON9_13/";
+#define COM9_14 "/sys/class/gpio/CON9_14/";
+#define COM9_15 "/sys/class/gpio/CON9_15/";
+#define COM9_16 "/sys/class/gpio/CON9_16/";
+#define COM9_17 "/sys/class/gpio/CON9_17/";
+#define COM9_18 "/sys/class/gpio/CON9_18/";
 
 extern int pinMode(char *pin, int mode);
 extern int digitalWrite(char *path, int isON);
+extern int digitalRead(char *path);
+
+    
