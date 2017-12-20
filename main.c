5f24d172d3b7c83735b64d356d11660b8a989dd1
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2017-12-20 12:23:19 +0900

    digitalReadのテスト

--- main.c
+++ main.c
@@ -9,22 +9,22 @@
 
 int main(void){
     int fd_red1 = -1, fd_red2 = -1, fd_green1 = -1, fd_green2 = -1, loop, status_code = 0;
-    char *path[1];
+    char *path[2];
     int status;
     
     path[0] = COM9_11;
+    path[1] = COM9_13;
     
     pinMode(path[0], OUTPUT);
+    pinMode(path[1], INPUT);
 
     while (1) {
-        digitalWrite(path[0], LOW);
-        status = digitalRead(path[0]);
-        printf("%d\r\n",status);
-        sleep(3);
-        digitalWrite(path[0], HIGH);
-        status = digitalRead(path[0]);
-        printf("%d\r\n",status);
-        sleep(3);
+        status = digitalRead(path[1]);
+        if(status == 1)
+            printf("OFF\r\n");
+        else
+            printf("ON\r\n");
+        sleep(1);
     }
     return status_code;
 } 
