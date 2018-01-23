65d2441d43ca498f14a36c38e576eb725b5cb9a0
unknown <admin@a1721311.ap.chugoku-pc.ac.jp>
2018-01-23 15:07:21 +0900

    GPIOControllerのモジュールの仕様変更

--- libraries/GPIOController.h
+++ libraries/GPIOController.h
@@ -9,17 +9,15 @@
 #define INPUT 1
 #define HIGH 1
 #define LOW 0
-#define COM9_11 "/sys/class/gpio/CON9_11/";
-#define COM9_12 "/sys/class/gpio/CON9_12/";
-#define COM9_13 "/sys/class/gpio/CON9_13/";
-#define COM9_14 "/sys/class/gpio/CON9_14/";
-#define COM9_15 "/sys/class/gpio/CON9_15/";
-#define COM9_16 "/sys/class/gpio/CON9_16/";
-#define COM9_17 "/sys/class/gpio/CON9_17/";
-#define COM9_18 "/sys/class/gpio/CON9_18/";
+#define COM9_11 "/sys/class/gpio/CON9_11/"
+#define COM9_12 "/sys/class/gpio/CON9_12/"
+#define COM9_13 "/sys/class/gpio/CON9_13/"
+#define COM9_14 "/sys/class/gpio/CON9_14/"
+#define COM9_15 "/sys/class/gpio/CON9_15/"
+#define COM9_16 "/sys/class/gpio/CON9_16/"
+#define COM9_17 "/sys/class/gpio/CON9_17/"
+#define COM9_18 "/sys/class/gpio/CON9_18/"
 
-extern int pinMode(char *pin, int mode);
-extern int digitalWrite(char *path, int isON);
-extern int digitalRead(char *path);
-
-    
+extern int pinMode(int pin, int isIN);
+extern int digitalWrite(int pin, int isON);
+extern int digitalRead(int pin);
