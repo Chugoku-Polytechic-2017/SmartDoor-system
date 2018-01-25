ab7004484c4d10082ba8e36ac9e4630ea142ecdf
石川諒 <ishikawa.pro@gmail.com>
2018-01-25 20:24:36 +0900

    gpioのパスの定義はatgpio.hに定義済みなので削除

--- libraries/GPIOController.h
+++ libraries/GPIOController.h
@@ -15,14 +15,6 @@
 #define INPUT 1
 #define HIGH 1
 #define LOW 0
-#define COM9_11 "/sys/class/gpio/CON9_11/"
-#define COM9_12 "/sys/class/gpio/CON9_12/"
-#define COM9_13 "/sys/class/gpio/CON9_13/"
-#define COM9_14 "/sys/class/gpio/CON9_14/"
-#define COM9_15 "/sys/class/gpio/CON9_15/"
-#define COM9_16 "/sys/class/gpio/CON9_16/"
-#define COM9_17 "/sys/class/gpio/CON9_17/"
-#define COM9_18 "/sys/class/gpio/CON9_18/"
 
 int pinMode(int pin, int isIN);
 int digitalWrite(int pin, int isON);
