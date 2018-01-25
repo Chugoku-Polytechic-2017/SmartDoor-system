2ed3dc1478c1e90c445c82ff3877925c5ab0f21e
ishikawa-pro <ihikawa.pro@gmail.com>
2018-01-25 18:38:04 +0900

    GPIOの割り込み処理を使った実装へ変更

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
-
 #include <stdio.h>
 #include "libraries/GPIOController.h"
 
-int main(void){
-    int status;
-    status = pinMode(11, OUTPUT);
-    status = pinMode(13, INPUT);
+static gboolean on_push(GIOChannel *ch, gpointer d) {
+    int *flag;
+    flag = d;
+    printf("on_push \r\n");
+    if (*flag == 0)
+        digitalWrite(11, HIGH);
+    else
+        digitalWrite(11, LOW);
 
-    while (1) {
-        status = digitalRead(13);
-        printf("%d\r\n",status);
-        if(status == 0) {                        //負論理
-            digitalWrite(11,LOW);
-            sleep(1);
-        }else{
-            digitalWrite(11,HIGH); 
-            sleep(1);        
-        }
-    }
+    *flag = ~*flag;
+
+    g_io_channel_read_to_end(ch, NULL, NULL, NULL);
+    return TRUE;
+}
+
+int main(void){
+    int status, flag = 0;
+    GMainLoop *loop;
+
+    loop = g_main_loop_new(NULL, FALSE);
+    status = pinMode(11, OUTPUT);
+    at_gpio_add(13,  AT_GPIO_EDGE_FALLING, on_push, &flag,  NULL);
+    g_main_loop_run(loop);
+    g_main_loop_unref(loop);
+
     return status;
 } 
