b93c69fb06a132008cad999585942285f4c506e1
ishikawa-pro <ihikawa.pro@gmail.com>
2018-01-25 18:37:22 +0900

    main.cでの多重インクルードを修正

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <poll.h>
 #include "libraries/GPIOController.h"
 
 int main(void){
